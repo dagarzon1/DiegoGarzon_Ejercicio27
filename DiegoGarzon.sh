0b35cb7654db246a8a40e9c679676f14fc83f482
da.garzon1 <da.garzon1@uniandes.edu.co>
2018-11-13 21:47:06 +0000

    update

---
+++ DiegoGarzon.sh
@@ -0,0 +1,11 @@
+!/bin/bash
+
+#PBS -l nodes=1:ppn=1,mem=1gb,walltime=00:10:00
+#PBS -M da.garzon1@uniandes.edu.co
+#PBS -m abe
+#PBS -N ejercicio27
+
+
+module load anaconda/python3
+cd $PBS_O_WORKDIR # este es el directorio desde donde se ejecuto qsub
+make
---
+++ makefile
@@ -0,0 +1,4 @@
+list1.x:list2.x
+    c++ list1.c -o list1.x
+list2.x:
+    c++ list2.c -o list2.x
