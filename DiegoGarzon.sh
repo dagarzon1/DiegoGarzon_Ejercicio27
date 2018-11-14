ac0b130524222c5c492271cea08f22ec4d7cc844
dagarzon1 <37335375+dagarzon1@users.noreply.github.com>
2018-11-13 19:29:00 -0500

    Update

--- DiegoGarzon.sh
+++ DiegoGarzon.sh
@@ -1,12 +1,13 @@
 !/bin/bash
 
-#PBS -l nodes=1:ppn=1,mem=1gb,walltime=00:10:00
+#PBS -l nodes=1:ppn=4,mem=32gb,walltime=00:10:00
 #PBS -M da.garzon1@uniandes.edu.co
 #PBS -m abe
 #PBS -N ejercicio27
 
 
 module load anaconda/python3
+make clean
 cd $PBS_O_WORKDIR # este es el directorio desde donde se ejecuto qsub
 make
 
